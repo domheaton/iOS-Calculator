9500ab7208f01236082f0f509708fedf4e6df38d
Dominic Heaton <dhh1g15@soton.ac.uk>
2018-10-30 10:47:45 +0000

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dominic Heaton on 29/06/2017.
+//  Copyright © 2017 Dominic Heaton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBOutlet weak var display: UILabel!
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get{
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+}
+
